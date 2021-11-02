1a1b47fd4e2fa1ad768ee98ef0c4c8558901e6e4
Diaconu Oana <oana.diaconu1@stud.ubbcluj.ro>
2021-11-02 21:52:58 +0200

    Crud

--- UserInterface/console.py
+++ UserInterface/console.py
@@ -1,5 +1,5 @@
-from Domain.rezervare import get_str
-from Logic.crud import create
+from Domain.rezervare import get_str, get_nume, get_clasa, get_pret, get_checkin, creeaza_rezervare
+from Logic.crud import create, read, update, delete
 
 
 def show_menu():
@@ -17,19 +17,50 @@ def handle_add(rezervari):
     return create(rezervari,id_rezervare,nume,clasa,pret,checkin)
 
 
+def handle_show_detalis(rezervari):
+    id_rezervare= int(input('Dati id-ul rezervarii pentru care doriti detalii: '))
+    rezervare= read(rezervari,id_rezervare)
+    print(f'Nume: {get_nume(rezervare)} ')
+    print(f'clasa: {get_clasa(rezervare)} ')
+    print(f'pret: {get_pret(rezervare)} ')
+    print(f'checkin: {get_checkin(rezervare)} ')
+
+
+def handle_update(rezervari):
+    id_rezervare = int(input('Dati id-ul rezervarii care se actualizeaza: '))
+    nume = input('Dati noul nume al rezervarii: ')
+    clasa = input('Alegeti noua clasa a rezervarii: ')
+    pret = input('Dati noul pret al  rezervarii: ')
+    checkin = input('Confirmati noul checkin al rezervarii: ')
+    return update(rezervari,creeaza_rezervare(id_rezervare,nume,clasa,pret,checkin))
+
+
+def handle_delete(rezervari):
+    id_rezervare = int(input('Dati id-ul rezervarii care se sterge: '))
+    rezervari = delete(rezervari,id_rezervare)
+    print('Stergerea a fost efectuata cu succes.')
+    return rezervari
+
 def handle_crud(rezervari):
     while True:
         print('1. Adaugare')
         print('2. Modificare')
         print('3. Stergere')
         print('a. Afisare')
+        print('d. Detalii rezervare')
         print('b. Revenire')
 
         optiune= input('Optiunea aleasa: ')
         if optiune == '1':
             rezervari = handle_add(rezervari)
+        elif optiune == '2':
+            rezervari = handle_update(rezervari)
+        elif optiune == '3':
+            rezervari = handle_delete(rezervari)
         elif optiune == 'a':
             handle_show_all(rezervari)
+        elif optiune == 'd':
+            handle_show_detalis(rezervari)
         elif optiune == 'b':
             break
         else:
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from Logic.crud import create
 from Tests.test_crud import test_create, test_read, test_update, test_delete
 from UserInterface.console import run_ui
 
 
 def main():
     rezervari = []
+    rezervari= create(rezervari, 8,'Gherman','bussines',4000,'da')
     rezervari = run_ui(rezervari)
 
 
